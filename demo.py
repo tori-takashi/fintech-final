9a2d7193cb2f42a8e52c88aa50e0ad81b1be9b80
tori-takashi <tori.toutoshi@gmail.com>
2019-10-24 02:54:00 +0800

    implement to download long term data

--- demo.py
+++ demo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83f8da6cf479a2d4959cd21d2b9e60624cc8bb0e7d30d375f9e20bf19c12d9ba
-size 745
+oid sha256:0b58e826d12abe6d03869a9f0f9bb1a493078afd70404700ae0c4a7d8ba9686e
+size 705
