6f5419484a087aa353985d14a9d18bd76185d055
tori-takashi <tori.toutoshi@gmail.com>
2019-10-23 23:47:50 +0800

    delete no need functions

--- lib/time_ms.py
+++ lib/time_ms.py
@@ -4,18 +4,5 @@ import calendar
 
 class TimeMS:
     @classmethod
-    def now(self):
-        now = datetime.utcnow()  # now UTC
-        unixtime_ms = calendar.timegm(now.utctimetuple())*1000  # Unix time
-        # get unix time and convert to milliseconds
-        return unixtime_ms
-
-    @classmethod
-    def to_unixtime(self, datetime_val):
-        unixtime_ms = calendar.timegm(
-            datetime_val.utctimetuple()) * 1000  # unix time
-        return unixtime_ms
-
-    @classmethod
     def fromtimestamp(self, ms):
         return datetime.fromtimestamp(ms/1000)
