1919c0eef57747564bf67d67f16703f8b2a9514d
tori-takashi <tori.toutoshi@gmail.com>
2019-10-23 22:26:13 +0800

    update function names

--- lib/time_ms.py
+++ lib/time_ms.py
@@ -3,11 +3,11 @@ import calendar
 
 
 class TimeMS:
-    def now_ms(self):
+    def now(self):
         now = datetime.utcnow()  # now UTC
         unixTime_ms = calendar.timegm(now.utctimetuple())*1000  # Unix time
         # get unix time and convert to milliseconds
         return unixTime_ms
 
-    def fromtimestamp_ms(self, ms):
+    def fromtimestamp(self, ms):
         return datetime.fromtimestamp(ms/1000)
