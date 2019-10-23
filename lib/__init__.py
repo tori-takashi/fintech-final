2188509f2c5e28c9d52cf269e8cbc01768075609
tori-takashi <tori.toutoshi@gmail.com>
2019-10-23 22:01:51 +0800

    add time manipulation functions

---
+++ lib/__init__.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:80ebfbf92f863da9ad089c8d11beefc6facf44afa1c7f3751febf64a62b24a05
+size 32
---
+++ lib/time_manipulation.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8649822211301daf982675c76744cdd701ab41f494f987749f8647298cffb087
+size 376
