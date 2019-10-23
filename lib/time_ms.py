d5b0b94b53777543135f80576b5c4ad22e08dcf6
tori-takashi <tori.toutoshi@gmail.com>
2019-10-23 22:26:13 +0800

    update function names

--- lib/time_ms.py
+++ lib/time_ms.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52ec91da4ecdd10dd71dbcec807505d38d8904a199c4c40209355d94f198eb5a
-size 366
+oid sha256:f0dbe36cad7226e0dc8c9a5e3e49e48c581c3597b0efa3bbdc0be61ee5a74661
+size 360
