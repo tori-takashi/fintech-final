70f3aaca1be85f1d2bffd047e0444c08d35f2f72
tori-takashi <tori.toutoshi@gmail.com>
2019-10-24 01:37:04 +0800

    update with sqlite3 demo

--- demo.py
+++ demo.py
@@ -1,17 +1,27 @@
 from datetime import datetime, timedelta
 
 import client.exchange_client as exchange_client
+import client.db_client as db_client
+
 from lib.time_ms import TimeMS
 from lib.pandamex import PandaMex
 
+import pandas as pd
+
 bitmex = exchange_client.ExchangeClient("bitmex").client
-now = datetime.now()
+db = db_client.DBClient("sqlite3")
+db_client = db.client
+db_cursor = db.cursor
+
+pdmex = PandaMex(bitmex)
 
 start_time = datetime.now() - timedelta(days=3)
 end_time = datetime.now() - timedelta(days=3) + timedelta(hours=1)
 
-pdmex = PandaMex(bitmex)
 df = pdmex.fetch_ohlcv("BTC/USD", "1m", start_time, end_time)
 ohlcv = PandaMex.to_timestamp(df)
 
-print(ohlcv)
+ohlcv.to_sql("ohlcv_data", db_client, if_exists="append", index=None)
+
+print(pd.read_sql_query("SELECT * FROM ohlcv_data", db_client))
+db_client.close()
