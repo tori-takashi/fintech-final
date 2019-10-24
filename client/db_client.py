bb4d267776282cbf265c71ad409fa4c93814a615
tori-takashi <tori.toutoshi@gmail.com>
2019-10-24 09:38:23 +0800

    add option to db client

--- client/db_client.py
+++ client/db_client.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e6660d258c9bbebbdcd411fcac321b566b736cdda59e9f6a6344e0a7380548d
-size 626
+oid sha256:f2fc81165db03310c54bf672bffcdd3f795edc7a2f80fcb4f8af1896413e20d9
+size 751
