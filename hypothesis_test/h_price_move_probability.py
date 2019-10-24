ad3c8ec548ab230536ed93b9944b9fe7ebe68db6
tori-takashi <tori.toutoshi@gmail.com>
2019-10-24 11:04:14 +0800

    use instance variable ohlcv

--- hypothesis_test/h_price_move_probability.py
+++ hypothesis_test/h_price_move_probability.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e75c041c2de72d07a8ebe3683810ac75873f32cc129c8ea0d26746f69c3bab54
-size 2358
+oid sha256:c0f80803d0b5a9ea06070b672e02391d80bfc734889b7fca763ad10e7628ae09
+size 2352
