00eef664fa91324be61c22350ba090408f1012ec
tori-takashi <tori.toutoshi@gmail.com>
2019-10-24 11:05:07 +0800

    update demo to test hypothesis

--- demo.py
+++ demo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b58e826d12abe6d03869a9f0f9bb1a493078afd70404700ae0c4a7d8ba9686e
-size 705
+oid sha256:72e14295b4061b743dbf0093630b3160582fdabede1cc0f0b0e200dac247519a
+size 550
