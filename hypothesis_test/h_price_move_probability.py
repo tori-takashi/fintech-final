d5a97326f58146021bf75d4a7dd915763adb76e6
tori-takashi <tori.toutoshi@gmail.com>
2019-10-24 11:11:50 +0800

    modify continuity valuation variable

--- hypothesis_test/h_price_move_probability.py
+++ hypothesis_test/h_price_move_probability.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0f80803d0b5a9ea06070b672e02391d80bfc734889b7fca763ad10e7628ae09
-size 2352
+oid sha256:4c310c2c425baf45a5ba74cfa391ccb03945288e8c5a35416bc87788e94bef03
+size 2362
