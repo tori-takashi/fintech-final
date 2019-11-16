b383bf7d6f99be7368c6677b8c51acabe0cd73ab
tori-takashi <tori.toutoshi@gmail.com>
2019-11-16 19:44:08 +0800

    add tuned bitmex-ws lib and update readme

---
+++ client/tuned_bitmex_websocket.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2f8bff8b93937e210af44bee4773db93a71dcc38330aa65cd82e480d43be87ae
+size 16025
---
+++ client/util/api_key.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:38f51aee0425f03df4867e4f1d751b6c2b7282ed7469decdf38519c958c093d0
+size 1240
