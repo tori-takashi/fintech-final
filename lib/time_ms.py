faad9937bd8886e003cb0a661cf4cccd00410ac0
tori-takashi <tori.toutoshi@gmail.com>
2019-11-17 11:43:43 +0800

    changed timems to use pandas.Timestamp

--- lib/time_ms.py
+++ lib/time_ms.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df7e6fc576ca21edba3c8ff79f8b366e9d7856b7f211a0e8f0bd1af66f36fdd1
-size 159
+oid sha256:768bf3cbc3c2753fac57a84328d8bf3ec2e15d81f47119e8f8dab64d6a7d4f73
+size 193
