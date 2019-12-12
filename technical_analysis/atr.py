2f71379af623dad56916a5e3fa456bdcc3a93908
tori-takashi <tori.toutoshi@gmail.com>
2019-12-12 16:39:10 +0800

    fix typo

--- technical_analysis/atr.py
+++ technical_analysis/atr.py
@@ -5,7 +5,7 @@ import talib as ta
 class TechnicalAnalysisAD:
     def __init__(self, df):
         # calculate and apply AD
-        self.ta_ATR = ta.AD(df["high"], df["low"], df["close"])
+        self.ta_ATR = ta.ATR(df["high"], df["low"], df["close"])
 
     def get_ta_atr(self):
-        return pd.DataFrame(self.ta_AD)
+        return pd.DataFrame(self.ta_ATR)
