d32d2b198b9c94f045d13ddb7576798f068cc59c
tori-takashi <tori.toutoshi@gmail.com>
2019-12-12 18:39:25 +0800

    remove debug message

--- lib/pandamex.py
+++ lib/pandamex.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46b7363d75a2001cb44bcbb200dc40abecb7bd8785bfb1dd8b8605224d481cfc
-size 3295
+oid sha256:cb7439bd2471d6bd7c8743a79dbca068886fda5d70a273a7e08bfd93559dc2ea
+size 3031
