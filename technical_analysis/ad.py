dbc1f2367ef00855ec71d9b9630300439743f13a
tori-takashi <tori.toutoshi@gmail.com>
2019-12-12 16:33:55 +0800

    simplify to use class

--- technical_analysis/ad.py
+++ technical_analysis/ad.py
@@ -1,23 +1,11 @@
 import pandas as pd
 import talib as ta
 
-data = pd.read_csv("ohlcv_with_future.csv")
 
-high = data["high"]
-low = data["low"]
-close = data["close"]
-volume = data["volume"]
-data["timestamp"] = pd.to_datetime(data["timestamp"])
+class TechnicalAnalysisAD:
+    def __init__(self, df):
+        # calculate and apply AD
+        self.ta_AD = ta.AD(df["high"], df["low"], df["close"], df["volume"])
 
-ta_AD = ta.AD(high, low, close, volume)
-output = pd.DataFrame(
-    data={
-        "high": high,
-        "low": low,
-        "close": close,
-        "volume": volume,
-        "timestamp": data["timestamp"],
-        "ta_AD": ta_AD}
-)
-output.reset_index(inplace=True, drop=True)
-output.to_csv("ta_ad.csv")
+    def get_ta_ad(self):
+        return pd.DataFrame(self.ta_AD)
--- technical_analysis/atr.py
+++ technical_analysis/atr.py
@@ -1,21 +1,11 @@
 import pandas as pd
 import talib as ta
 
-data = pd.read_csv("ohlcv_with_future.csv")
 
-high = data["high"]
-low = data["low"]
-close = data["close"]
-data["timestamp"] = pd.to_datetime(data["timestamp"])
+class TechnicalAnalysisAD:
+    def __init__(self, df):
+        # calculate and apply AD
+        self.ta_ATR = ta.AD(df["high"], df["low"], df["close"])
 
-ta_ATR = ta.ATR(high, low, close)
-output = pd.DataFrame(
-    data={
-        "high": high,
-        "low": low,
-        "close": close,
-        "timestamp": data["timestamp"],
-        "ta_ATR": ta_ATR}
-)
-output.reset_index(inplace=True, drop=True)
-output.to_csv("ta_atr.csv")
+    def get_ta_atr(self):
+        return pd.DataFrame(self.ta_AD)
