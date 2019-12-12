7f23a1630a5ba37bbaafb265a3f357fc2864d830
tori-takashi <tori.toutoshi@gmail.com>
2019-12-12 13:54:11 +0800

    add ad, atr

---
+++ technical_analysis/ad.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:96c944ad52cb822a5832174ebcd853ea3375d097331465ec0bbaf368610ad53b
+size 527
---
+++ technical_analysis/atr.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:64d33df4bed927bbdc25f55baa3026412dd24b14b3597114bf78de72cf38aa10
+size 474
