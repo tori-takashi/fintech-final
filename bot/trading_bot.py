4bf70f0fcb8e211b4a868defb307884316843751
tori-takashi <tori.toutoshi@gmail.com>
2019-12-14 04:31:22 +0800

    add log outpur, backtest functions

--- bot/trading_bot.py
+++ bot/trading_bot.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3e21deb4028e9f79f67a15350650bd262192c3417779f18bba8b7ac1b8180b0
-size 5196
+oid sha256:397c0ad1a08b8efffb4ae6665321f502ff8dd1c3db390ab1ab61f5942949c587
+size 11450
