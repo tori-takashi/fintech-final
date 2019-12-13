53369537a4e0e79931f693cabd3d49acdbaf43fc
tori-takashi <tori.toutoshi@gmail.com>
2019-12-14 06:07:47 +0800

    add day to demo

--- demo.py
+++ demo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:961c4f62a01cbd9f5b87c0131aed73f53673fdf99e2709dba0b1a54578d6caf3
-size 740
+oid sha256:5550a971817af8b5d8672a482fcbe2fb9a80af5175d5d8c627d9bdb2d178bb6a
+size 756
