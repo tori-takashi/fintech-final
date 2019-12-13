645d9b7bd7598b72941c14858f65de17a32c464a
tori-takashi <tori.toutoshi@gmail.com>
2019-12-14 04:43:38 +0800

    fix missing

--- bot/trading_bot.py
+++ bot/trading_bot.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a7d86e333ec96a1ab619c5ad2bb7762001e24907637329e98d4aef2ffc3a3f0
-size 11728
+oid sha256:6e4cb73edfa9bccf0e77de5ba8224228b767da378cfa033ef583b075b5b4b45a
+size 11749
