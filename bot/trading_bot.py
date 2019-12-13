56a80372ea9e4354fbc044bc248073347745d8bd
tori-takashi <tori.toutoshi@gmail.com>
2019-12-14 04:42:47 +0800

    change log dir, add duration config

--- bot/trading_bot.py
+++ bot/trading_bot.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:397c0ad1a08b8efffb4ae6665321f502ff8dd1c3db390ab1ab61f5942949c587
-size 11450
+oid sha256:1a7d86e333ec96a1ab619c5ad2bb7762001e24907637329e98d4aef2ffc3a3f0
+size 11728
