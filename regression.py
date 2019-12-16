6c57c63ba7e361dc0cb693602d316bc6772c3d22
tori-takashi <tori.toutoshi@gmail.com>
2019-12-16 21:49:56 +0800

    indent

--- regression.py
+++ regression.py
@@ -16,39 +16,44 @@ from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import accuracy_score
 
-data = pd.read_csv('ohlcv_with_future.csv')
+data = pd.read_csv('csv/ohlcv_with_future.csv')
 
-col = ['trend','RSI','close','Fast_Line','Slow_Line_9','close_c_1min','close_1min','close_10min']
+col = ['trend', 'RSI', 'close', 'Fast_Line', 'Slow_Line_9',
+       'close_c_1min', 'close_1min', 'close_10min']
 
 for c in col:
     data = data[pd.notnull(data[c])]
 
 data['trend'] = pd.Categorical(data['trend'])
-dfDummies = pd.get_dummies(data['trend'], prefix = 'trend')
+dfDummies = pd.get_dummies(data['trend'], prefix='trend')
 data = pd.concat([data, dfDummies], axis=1)
-    
-x_data = data[['close','Fast_Line','Slow_Line_9','trend_uptrend','trend_downtrend','RSI']].copy()
+
+x_data = data[['close', 'Fast_Line', 'Slow_Line_9',
+               'trend_uptrend', 'trend_downtrend', 'RSI']].copy()
 y_data = data[['close_10min']].copy()
 
-#normalise
+# normalise
 for i in x_data.columns:
     x_data[i] = (x_data[i] - x_data[i].mean()) / x_data[i].std()
-    
-x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2, random_state=1)
-x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2, random_state=1)
 
-#linear regression
+x_train, x_test, y_train, y_test = train_test_split(
+    x_data, y_data, test_size=0.2, random_state=1)
+x_train, x_val, y_train, y_val = train_test_split(
+    x_train, y_train, test_size=0.2, random_state=1)
+
+# linear regression
 model = linear_model.LinearRegression()
-model.fit(x_train,y_train)
+print(y_train)
+model.fit(x_train, y_train)
 predictions = model.predict(x_test)
 
 print(predictions[0:5])
 
-plt.scatter(y_test, predictions, color = 'green')
-plt.title('Linear Regression') 
+plt.scatter(y_test, predictions, color='green')
+plt.title('Linear Regression')
 plt.xlabel("True Values")
 plt.ylabel("Predictions")
-plt.show() 
+plt.show()
 print("Score:", model.score(x_test, y_test))
 
 validation_scores = cross_val_score(model, x_data, y_data, cv=6)
@@ -57,8 +62,7 @@ print("Cross-validated scores:", validation_scores)
 #print("accuracy : ", accuracy_score(y_test, predictions))
 
 
-
-#polynomial regression
+# polynomial regression
 
 #model2 = linear_model.LinearRegression()
 #model2.fit(x_train, y_train)
@@ -66,14 +70,14 @@ print("Cross-validated scores:", validation_scores)
 #plt.plot(y_test, predictions2)
 #plt.scatter(y_test, predictions2)
 #plt.title("Linear Model, Polynomial Degree = 1")
-#plt.show()
+# plt.show()
 #
-#poly_features = PolynomialFeatures(degree = 2)  
+#poly_features = PolynomialFeatures(degree = 2)
 #X_poly = poly_features.fit_transform(x_train)
-#poly_model = LinearRegression()  
+#poly_model = LinearRegression()
 #poly_model.fit(X_poly, y_train)
 #pred = poly_model.predict(X_poly)
 #plt.plot(y_test, predictions2)
 #plt.scatter(y_test, predictions2)
 #plt.title("Polynomial Degree = 2")
-#plt.show()
+# plt.show()
