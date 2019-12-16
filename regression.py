dbbd2a4718a64ad138e31006841cccddd063c773
tori-takashi <tori.toutoshi@gmail.com>
2019-12-16 21:49:56 +0800

    indent

--- regression.py
+++ regression.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2223b8cc1ddf96a0ba2184e4c560a7435df99cabe6237894a006f977cd248bbf
-size 2396
+oid sha256:1dab2dd1d434828ebc09e12b5b2598495af71a88b6c30e508c9552c02b7d9580
+size 2447
