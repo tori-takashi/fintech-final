87646c4ff3b132436f0969971afc7e146f932107
tori-takashi <tori.toutoshi@gmail.com>
2019-12-17 00:14:14 +0800

    refactoring

--- bot/bottom_trend_follower.py
+++ bot/bottom_trend_follower.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8b1ea7d0a0a7358b9ab65f4994ef0c030424ed1bcacb2b08c8206662c14b2cf
-size 4548
+oid sha256:f3ab7dcc12909ea13d8aa8eaee115e1bcee5e3b25698e338db02fe74deaface9
+size 3141
