d90d190d99abd36e417fb53d4866f06da3483493
tori-takashi <tori.toutoshi@gmail.com>
2019-12-17 18:05:07 +0800

    add db writing function

--- client/db_client.py
+++ client/db_client.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2fc81165db03310c54bf672bffcdd3f795edc7a2f80fcb4f8af1896413e20d9
-size 751
+oid sha256:be3cc9fa3af282fe4b8b0d9da78d1f8c0a4ef4d4ea7864911aedaa61dc42a960
+size 1143
