997d14955042e493643cd6f270c0531323c10cd2
tori-takashi <tori.toutoshi@gmail.com>
2019-12-17 18:12:25 +0800

    rename function

--- build_dataset.py
+++ build_dataset.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b3379e4bb18a4a5824b01d16d3618dc143b68b084574691e53c3ce4c8cadf0d
+oid sha256:8380000a90f28cdbce9febb531b32972683aee85ac5c5647eea4d04058729d25
 size 468
