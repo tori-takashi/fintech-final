3b096a760610e01e8dc590fe28183c458c443deb
tori-takashi <tori.toutoshi@gmail.com>
2019-12-18 21:50:44 +0800

    remove drop index

--- lib/dataset.py
+++ lib/dataset.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b8997565f08a9043ab7b8f07ae31ebdfef0d852583bee3ab7e2d85450ae9ee3
-size 3210
+oid sha256:0311592b3f5c998e10f018447c1eb88d2ff3d06b5b366de692a1e2b76b09b456
+size 3149
