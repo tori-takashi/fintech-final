de7202b7762865ea68204ee5fe8c47e39f9cb5ed
tori-takashi <tori.toutoshi@gmail.com>
2019-12-18 21:58:17 +0800

    split sqlite3 functions

--- client/db_client.py
+++ client/db_client.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:971d9124bf6d3c0419e0a8bf8308bbd4f01f7d763fe202f721b811c3055dccc2
-size 3079
+oid sha256:99f5abdb3883eb7736096b1af8ea2083c2d85bc5792bcb97aae81b78fc4f0308
+size 3246
