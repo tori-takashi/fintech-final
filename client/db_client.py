e3668737bc52d0c02acc50c3039b6664eb2cea70
tori-takashi <tori.toutoshi@gmail.com>
2019-12-18 13:12:43 +0800

    add table exist checker

--- client/db_client.py
+++ client/db_client.py
@@ -1,6 +1,7 @@
 from configparser import SafeConfigParser
 import sqlite3
 import pandas as pd
+from sqlalchemy import create_engine
 
 
 class DBClient:
@@ -10,24 +11,57 @@ class DBClient:
 
         # config.ini should be same place to executing file
         self.config.read("config.ini")
-        self.client = self.establish_connection_to_db(db_type)
-        self.cursor = self.client.cursor()
+        self.db_type = db_type
 
-    def establish_connection_to_db(self, db_type):
-        if db_type == "sqlite3":
+        self.engine = self.establish_connection_to_db()
+
+    def establish_connection_to_db(self):
+        if self.is_sqlite3():
             return self.sqlite3_establish_connection()
 
     def sqlite3_establish_connection(self):
         if self.opt == None:
-            return sqlite3.connect(self.config['sqlite3']['db_path'])
+            return create_engine("sqlite3:///" + self.config['sqlite3']['db_path'], echo=True)
         else:
-            return sqlite3.connect(self.opt)
+            return create_engine("sqlite3:///" + self.opt)
 
     def write_to_table(self, table_name, dataframe, if_exists):
-        dataframe.to_sql(table_name, self.client, if_exists=if_exists)
+        dataframe.to_sql(table_name, self.client,
+                         if_exists=if_exists, index=False)
 
     def overwrite_to_table(self, table_name, dataframe):
         self.write_to_table(table_name, dataframe, if_exists="replace")
 
     def append_to_table(self, table_name, dataframe):
         self.write_to_table(table_name, dataframe, if_exists="append")
+
+    def is_table_exist(self, table_name):
+        query = "SELECT count(*) FROM sqlite_master WHERE type='table' AND name='{}';".format(table_name)
+        results = self.exec_sql(table_name, query, False)
+        for result in results:
+            if result == (1,):
+                return True
+            else:
+                return False
+
+    def get_last_row(self, table_name):
+        if self.is_sqlite3():
+            query = "SELECT * FROM " + table_name + " WHERE index = last_insert_rowid();"
+        self.exec_sql(table_name, query)
+
+    def exec_sql(self, table_name, query, return_df=True):
+        result_rows = self.cursor.execute(query)
+        if return_df:
+            column_names = self.get_column_name(table_name)
+            return pd.DataFrame(data=result_rows, columns=column_names)
+        else:
+            return result_rows
+
+    def get_column_name(self, table_name):
+        if self.is_sqlite3():
+            query = "SELECT * FROM " + table_name + ";"
+            result = self.cursor.execute(query)
+            return result.description
+
+    def is_sqlite3(self):
+        return self.db_type == "sqlite3"
