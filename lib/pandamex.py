6f5b957121e0f5466f87fe985e323756313eb9e9
tori-takashi <tori.toutoshi@gmail.com>
2019-12-18 12:56:16 +0800

    add no index option, use exchange client not client

--- lib/pandamex.py
+++ lib/pandamex.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8e88009186c7256595479a10aac1f08eb165f2043202c6b1124d2cc0d63c6da
-size 3041
+oid sha256:15257f803214f87be8d843f7d070d16a8154f9f7b6baf0dab1c90ce4a2932960
+size 3150
