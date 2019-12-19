31230a176fd15bdcdfee36c410c4123d63529def
tori-takashi <tori.toutoshi@gmail.com>
2019-12-19 22:32:47 +0800

    fix getting error when ohlcv db is empty

--- client/db_client.py
+++ client/db_client.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28ad0115bcc3a6d9ebb85f1fcfb91eec00cc5e32772f6cdc55b479265f945470
-size 2960
+oid sha256:f0bd5927c4154455aa17715b4663a6aef6085f09fce93fb32783896448066a42
+size 3043
--- lib/dataset.py
+++ lib/dataset.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:352a09e23accc142b7b0b4d69befd0e4a64e3289764864aa1955060ca580b226
-size 4375
+oid sha256:3cf0f7004d99578efe02dd0d894c7eb9abc42820beafc5a3b59ca133875b0e56
+size 4442
