c18422eb0af76160223f9ae1bbb6be7e26ddacf4
tori-takashi <tori.toutoshi@gmail.com>
2019-12-19 15:56:21 +0800

    fix iteration bug, adhock patch for timezone

--- lib/pandamex.py
+++ lib/pandamex.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15257f803214f87be8d843f7d070d16a8154f9f7b6baf0dab1c90ce4a2932960
-size 3150
+oid sha256:721deea6ba5710ba5e14c337501e873ab7f741d9e531f44c98cc7278ec15dfff
+size 3325
