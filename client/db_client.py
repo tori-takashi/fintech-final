1d99d7e0e88ea101e2514b1c2c7c91cd4c7325b0
tori-takashi <tori.toutoshi@gmail.com>
2019-12-19 15:57:25 +0800

    change to return pandas.Series for returning 1 row function, add index col designation

--- client/db_client.py
+++ client/db_client.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5f03017a671d53aaa7664096df701f3b8d9ce9311f46c1cbda780bd43e9b5be
-size 2868
+oid sha256:28ad0115bcc3a6d9ebb85f1fcfb91eec00cc5e32772f6cdc55b479265f945470
+size 2960
