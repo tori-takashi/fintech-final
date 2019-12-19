b2ac204e914a56cd1eec2ad5e45ceb8f7a630676
tori-takashi <tori.toutoshi@gmail.com>
2019-12-19 10:08:18 +0800

    split sqlite3 and influxdb

--- client/db_client.py
+++ client/db_client.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99f5abdb3883eb7736096b1af8ea2083c2d85bc5792bcb97aae81b78fc4f0308
-size 3246
+oid sha256:2d0302981eb5e21d18b93fe2c040a43244b8a00604e589f27ed6ea5d3db07123
+size 3295
