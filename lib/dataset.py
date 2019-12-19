4dd8b604672f132247362a5d9319c77f6c336504
tori-takashi <tori.toutoshi@gmail.com>
2019-12-19 11:15:00 +0800

    create table with sqlalchemy

--- lib/dataset.py
+++ lib/dataset.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0311592b3f5c998e10f018447c1eb88d2ff3d06b5b366de692a1e2b76b09b456
-size 3149
+oid sha256:a1e4078fa2edd0febe6ad6edc10f68874bdef9be00e63c62b0923df134a2b76c
+size 2953
--- model/ohlcv_1min.py
+++ model/ohlcv_1min.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ca97093c2995a1b73ce6db0c95144e62854083255c855dbfe0320042d69bc87
-size 398
+oid sha256:1994de1508fcc05ca968f0e717d223e25643a6e441c97591f7b6a7e5a05fecd0
+size 753
