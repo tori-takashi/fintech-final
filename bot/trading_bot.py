ee1ce1f35a6755418e1c9242eeeacad73195e1ff
tori-takashi <tori.toutoshi@gmail.com>
2019-12-21 02:43:34 +0800

    add other metrics

--- bot/trading_bot.py
+++ bot/trading_bot.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54239ebba46b3d8e4585b6611eadc4fcdbc02b0566a67c4aeb93c6e652b95987
-size 29652
+oid sha256:689966a8c0cd261d2997cf8076dd3ac01d2e4342fcaba81018abc0e1a0779beb
+size 31269
