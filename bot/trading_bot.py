90dd6c4267376092c34ab1de42827d7f366f4b19
tori-takashi <tori.toutoshi@gmail.com>
2019-12-20 23:23:32 +0800

    add profit order

--- bot/trading_bot.py
+++ bot/trading_bot.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68a367cf607a93d6f1806510e1517112f632146190c1d90ade3736a2b11c0522
-size 27900
+oid sha256:54239ebba46b3d8e4585b6611eadc4fcdbc02b0566a67c4aeb93c6e652b95987
+size 29652
--- model/backtest_summary.py
+++ model/backtest_summary.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6ac64ab9a790c81181e89b2ac5cd0964250f64c0f091ddea8590998613a6f5b
-size 17572
+oid sha256:2083a4f1ae32a734503392b987d494f2e0dfc7cbb39c26218a20ae7954c550ed
+size 18858
