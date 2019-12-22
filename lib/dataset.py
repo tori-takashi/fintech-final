e6ca169e26c9b0b0eb2ecf59a7fcc44d08cdc90b
tori-takashi <tori.toutoshi@gmail.com>
2019-12-22 18:46:24 +0800

    change to use pydatetime

--- lib/dataset.py
+++ lib/dataset.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b2bf475eca9b36d49236b57a628e672bb0f1b880cddd1048e2eee08dd9d1473
-size 4482
+oid sha256:a00c2833f6c48530f973db4c9844f25ac957e213a059403bd8c394c208953b6a
+size 4491
