ec6048b12b2048e4674a88aa81814a2ca50b4a26
tori-takashi <tori.toutoshi@gmail.com>
2019-12-22 23:17:26 +0800

    remove needless code from run_backtest()

--- bot/trading_bot.py
+++ bot/trading_bot.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6dc6e60dabc4705db95ad8de2610b144220b3e4f18aaa81b08513d1ba2459a08
-size 26614
+oid sha256:dd716b27d40e537baa9f2b59bb1e4cd4ac7bfff95c8aa8b8261f007151a23384
+size 25409
