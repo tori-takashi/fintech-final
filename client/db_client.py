29b143b8892232c2c46de17496444e9929a3da9e
tori-takashi <tori.toutoshi@gmail.com>
2019-12-23 13:51:47 +0800

    add mysql execsql, is_table_exist

--- client/db_client.py
+++ client/db_client.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee4a2505ec8d1778fac3dfccd78d91adda028cb8fddfa76771e6545404f5c6e3
-size 3759
+oid sha256:11e1d83016616751aff477bbb97003b7056fb28e3f2e7fb0867d7e91cdafb7e8
+size 4540
