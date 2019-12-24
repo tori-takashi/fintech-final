b8b20d3b2a5db20440b691ffde0adcb07a221641
tori-takashi <tori.toutoshi@gmail.com>
2019-12-25 05:01:17 +0800

    remove copy and pasted needless repr() code

--- lib/dataset.py
+++ lib/dataset.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c756034e810240feeddd317b86996c70741b469ccadad3730f70c608bcdb0df
-size 6004
+oid sha256:685d9394261ae55be3613888626e448ed24503395aec439ceb246bd010bf0b03
+size 5887
