dd4e24ed0d9cb91725ace3b410cf69f356975146
tori-takashi <tori.toutoshi@gmail.com>
2019-12-26 21:01:43 +0800

    update README and build_dataset.py

--- build_dataset.py
+++ build_dataset.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8380000a90f28cdbce9febb531b32972683aee85ac5c5647eea4d04058729d25
-size 468
+oid sha256:4fb0e3563b37872f90d92d95988cc2b32582e9cb293dc023dd0cdeb35b714e4c
+size 530
