ef1407fc73820b401905ccc6be1d8898e475ef45
tori-takashi <tori.toutoshi@gmail.com>
2019-12-26 12:27:55 +0800

    add backtest start / end time

--- model/backtest_summary.py
+++ model/backtest_summary.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7395ff087425a89691c1664904947c86f182cf1b9408e6c30edc77d5c4365b6e
-size 18755
+oid sha256:23081a868e55494edb22cac002036b12b1deb57d600bd4de40bf86b9d37ed3cd
+size 18978
