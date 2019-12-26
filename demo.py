2929723a9c1a510dce46050179beb6b9f43a5e21
tori-takashi <tori.toutoshi@gmail.com>
2019-12-26 20:26:02 +0800

    demo.py

--- demo.py
+++ demo.py
@@ -8,6 +8,7 @@ from client.db_client import DBClient
 from client.exchange_ws_client import WSClient
 from lib.pandamex import PandaMex
 from lib.time_ms import TimeMS
+from lib.dataset import Dataset
 
 from hypothesis_test.fetch_high_frequency_data_test import FetchHighFrequencyData
 from hypothesis_test.h_price_move_probability import HPriceMovePlobability
@@ -16,5 +17,18 @@ from hypothesis_test.volatility_dependent_offset_test import VolatilityDependent
 from bot.bottom_trend_follower import BottomTrendFollow
 
 bitmex_exchange_client = ExchangeClient("bitmex")
-bot_bot = BottomTrendFollow(bitmex_exchange_client, is_backtest=True)
-bot_bot.run(duration_days=90)
+
+# influx_client = DBClient("influxdb")
+# print(influx_client.connector.get_list_database())
+
+sqlite3_client = DBClient("sqlite3")
+mysql_client = DBClient("mysql")
+
+# update database
+dataset_manager = Dataset(mysql_client, bitmex_exchange_client)
+download_start_time = datetime.now() - timedelta(days=1000)
+dataset_manager.update_ohlcv("bitmex", download_start_time)
+
+bot_bot = BottomTrendFollow(
+    db_client=mysql_client, exchange_client=bitmex_exchange_client, is_backtest=True)
+bot_bot.run()
