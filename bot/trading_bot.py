5e02eb994a52e760a3fc3f1a060d8064a5d0f425
tori-takashi <tori.toutoshi@gmail.com>
2019-12-26 20:21:08 +0800

    v1.00, update summary columns, remove useless columns, implement drawdowns and other metrics

--- bot/trading_bot.py
+++ bot/trading_bot.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26f3beabbe9c52bb5ae256f3b30ddf6fc35905e6f0f5b09809ffae780c513657
-size 31047
+oid sha256:8e1a4f3c124b743a4a3e277c68ad1aa28248e7d0f24a8405e6f6a3890b6408ff
+size 32247
--- model/backtest_summary.py
+++ model/backtest_summary.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59c3cb42f8ebf14f6a028464f3049ffb5bec0b0de6872ac238b28c93c3f2e143
-size 19141
+oid sha256:f78c75a0aa252c15e5ea004d6618f6791d061b77c58b47fd8f6719ddea02cb82
+size 18121
