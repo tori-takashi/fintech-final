6071cead6d31134ac6d59df315cbfd7d6989d7e0
tori-takashi <tori.toutoshi@gmail.com>
2019-12-26 12:51:23 +0800

    add protorype of timestamp str column

--- lib/pandamex.py
+++ lib/pandamex.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e44d0c54eebc139d1a39b5faba670cf094b538b6270430723ea8c599d9f645b
-size 3912
+oid sha256:aaf5438ff314932dd36c0ec45398dde85718fc8108103fdf7325ecb0d1b4f586
+size 4073
