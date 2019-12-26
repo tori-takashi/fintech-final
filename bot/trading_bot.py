87f537cbe162454e4e3bb0d6c8843a74208139fc
tori-takashi <tori.toutoshi@gmail.com>
2019-12-26 20:22:05 +0800

    fix indent

--- bot/trading_bot.py
+++ bot/trading_bot.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e1a4f3c124b743a4a3e277c68ad1aa28248e7d0f24a8405e6f6a3890b6408ff
-size 32247
+oid sha256:d62fd164cced360aadac6d4f63fd1ff676e5c964018578f81d60646e9a0cc68c
+size 32237
