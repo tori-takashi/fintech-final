cf3cb2a3cb3b2e44d8bb7d83476f84690dc3f358
tori-takashi <tori.toutoshi@gmail.com>
2019-12-26 20:39:02 +0800

    add reset function for optimization, remove needless code

--- bot/trading_bot.py
+++ bot/trading_bot.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d62fd164cced360aadac6d4f63fd1ff676e5c964018578f81d60646e9a0cc68c
-size 32237
+oid sha256:184025cd275ce3322e53807b7f4294f2885343e629d85b9ff5184a59e266d65f
+size 32791
