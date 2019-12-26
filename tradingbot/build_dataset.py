01b1428c854f5aa9d304654a4ee1b4caca262d51
tori-takashi <tori.toutoshi@gmail.com>
2019-12-26 23:40:59 +0800

    update build_dataset

--- tradingbot/build_dataset.py
+++ tradingbot/build_dataset.py
@@ -1,16 +1,18 @@
 from datetime import datetime, timedelta
+from pathlib import Path
 
 from lib.dataset import Dataset
 
 from client.db_client import DBClient
 from client.exchange_client import ExchangeClient
 
-bitmex_exchange_client = ExchangeClient("bitmex")
-mysql_client = DBClient("mysql")
+bitmex_exchange_client = ExchangeClient(
+    "bitmex", Path("tradingbot/config.ini"))
+mysql_client = DBClient("mysql", Path("tradingbot/config.ini"))
 
 dataset_manager = Dataset(mysql_client, bitmex_exchange_client)
 
-start_time = datetime.now() - timedelta(days=100)
+start_time = datetime.now() - timedelta(days=1000)
 end_time = datetime.now()
 
 dataset_manager.update_ohlcv("bitmex", start_time)
