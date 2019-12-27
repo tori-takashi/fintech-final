06f5ff9e73261119e7ca0a532e6cae4da272df76
tori-takashi <tori.toutoshi@gmail.com>
2019-12-27 14:53:26 +0800

    add research kit

--- research_notebook/research_kit.py
+++ research_notebook/research_kit.py
@@ -6,15 +6,22 @@ from datetime import datetime, timedelta
 from pprint import pprint
 
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 from client.exchange_client import ExchangeClient
 from client.db_client import DBClient
 from client.exchange_ws_client import WSClient
+
 from lib.pandamex import PandaMex
 from lib.time_ms import TimeMS
 from lib.dataset import Dataset
+
 from bot.bottom_trend_follower import BottomTrendFollow
+
+from model.backtest_management import BacktestManagement
+from model.backtest_transaction_log import BacktestTransactionLog
+from model.backtest_summary import BacktestSummary
 
 bitmex_exchange_client = ExchangeClient(
     "bitmex", Path("../tradingbot/config.ini"))
