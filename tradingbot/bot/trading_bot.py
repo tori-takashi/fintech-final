471d804dd1353542e829ca881a31cd5e6385b1ba
tori-takashi <tori.toutoshi@gmail.com>
2019-12-28 20:23:09 +0800

    optimization with bulk insert

--- tradingbot/bot/trading_bot.py
+++ tradingbot/bot/trading_bot.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1500fe2a0b25775b9d61a972b1aaf61591a53aaa6a136834151ccb6295fe416
-size 33064
+oid sha256:85af1f833ecab40f925a30a048f9b1b028fee5f5d179aeea71450f8cbc1c8b91
+size 32182
