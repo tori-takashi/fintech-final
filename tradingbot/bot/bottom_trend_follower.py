690883592763ee8a3ec5a9643075e4a0c27e4090
tori-takashi <tori.toutoshi@gmail.com>
2019-12-28 22:27:36 +0800

    add comment that no used function

--- tradingbot/bot/bottom_trend_follower.py
+++ tradingbot/bot/bottom_trend_follower.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d19cef6aa328c675d47109e44706e3aa8b277d3d29fc311df00691a2a600e3e5
-size 3790
+oid sha256:05d2f68bae8eb34a64ef0e6e905c3006129720c592f6e81d01166e653ca009a4
+size 3821
