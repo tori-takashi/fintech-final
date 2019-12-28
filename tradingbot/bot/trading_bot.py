5791fedcb370affb5ff2f642925e6aa0525c66a8
tori-takashi <tori.toutoshi@gmail.com>
2019-12-28 19:48:34 +0800

    fix win consecutive, update for bulk insert

--- tradingbot/bot/trading_bot.py
+++ tradingbot/bot/trading_bot.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80976629c60cfefee91e2c06fc6fdd392666c0a15086bd90c4657c2b95aa39d5
-size 32958
+oid sha256:c1500fe2a0b25775b9d61a972b1aaf61591a53aaa6a136834151ccb6295fe416
+size 33064
