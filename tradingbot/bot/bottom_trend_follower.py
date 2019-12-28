97b28df904982cd9ab7fdf009fdd08d392523228
tori-takashi <tori.toutoshi@gmail.com>
2019-12-28 16:40:43 +0800

    optimization with map

--- tradingbot/bot/bottom_trend_follower.py
+++ tradingbot/bot/bottom_trend_follower.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a542bf459b796bece36d0a7fa30195f47cfbb77fef1b5e3fd490850ff6ca8d8
-size 3829
+oid sha256:9c3d409b0289b01d73ddc74e5fcf4a9661c5fe9883f959f56de1c738b2ca9e7c
+size 3826
