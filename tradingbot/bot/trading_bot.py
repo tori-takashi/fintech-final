914dbe652de2c1cc475970d75a5d341c1ba68c95
tori-takashi <tori.toutoshi@gmail.com>
2019-12-28 17:05:27 +0800

    add transactions length checker

--- tradingbot/bot/trading_bot.py
+++ tradingbot/bot/trading_bot.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd88386f14daa56808e0e3c5d7696eb22253708df12749643c607c2eff3186d1
-size 32892
+oid sha256:80976629c60cfefee91e2c06fc6fdd392666c0a15086bd90c4657c2b95aa39d5
+size 32958
