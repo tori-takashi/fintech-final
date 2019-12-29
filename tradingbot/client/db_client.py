e77782eb69b90395bdf62f360688c6867e9598a6
tori-takashi <tori.toutoshi@gmail.com>
2019-12-29 20:48:01 +0800

    add convenient getrow method

--- tradingbot/client/db_client.py
+++ tradingbot/client/db_client.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b5c1d9ca6ad0b589b88b50ed432cde9d435d5398540e065e7d1ab4a5bad823f
-size 3552
+oid sha256:b91935b148bdfbf432f4bfbc2833ee97a70a061667a26cac81f01772ba2990a6
+size 3917
