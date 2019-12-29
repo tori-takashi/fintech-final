eca92a82bb0f22c7200bd64c19b7a2d86b8e65b1
tori-takashi <tori.toutoshi@gmail.com>
2019-12-29 11:40:01 +0800

    update inefficient SQL

--- tradingbot/client/db_client.py
+++ tradingbot/client/db_client.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84749536705676460bfeed98cc056444ae92176c60d1de94006aa35f4ce31f49
-size 3400
+oid sha256:7b5c1d9ca6ad0b589b88b50ed432cde9d435d5398540e065e7d1ab4a5bad823f
+size 3552
--- tradingbot/lib/dataset.py
+++ tradingbot/lib/dataset.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fb6d311441a632c2dbbeb7cabfd976623a3d979971f9afd8c2fceb7a078e46b
-size 5736
+oid sha256:3e3ed59f2236e018f8b04659313b89e02099af2690d8b3803f7baa63ef0df064
+size 5929
