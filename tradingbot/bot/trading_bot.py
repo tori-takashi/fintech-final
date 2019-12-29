7e69c3c68753372276e21e134e3146bc59198961
tori-takashi <tori.toutoshi@gmail.com>
2019-12-29 12:27:03 +0800

    fix missing space, remove needless table function, remove debug message

--- tradingbot/bot/trading_bot.py
+++ tradingbot/bot/trading_bot.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e19433959c053cb1157e2bfae06a12ce2df91e6f322a4f1780b685a288c00a0
-size 32156
+oid sha256:a1c9677b63bb451062c4039e5de4d7be799c7a767b7e9e27d3d681c00138ddcc
+size 32317
