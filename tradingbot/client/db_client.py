441e2d866e9d7503818670acc7f3e17ca71fe9a2
tori-takashi <tori.toutoshi@gmail.com>
2019-12-29 20:50:13 +0800

    fix forgot correcting

--- tradingbot/client/db_client.py
+++ tradingbot/client/db_client.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b91935b148bdfbf432f4bfbc2833ee97a70a061667a26cac81f01772ba2990a6
-size 3917
+oid sha256:e40397e589b69b6bacb1ef1e58adff3c2116a52c49676367e7dd744c58100355
+size 3892
