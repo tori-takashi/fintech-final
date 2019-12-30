cf625177a1c3e691b8a0b4ace1d800085223cbd3
tori-takashi <tori.toutoshi@gmail.com>
2019-12-30 15:16:58 +0800

    reorder to title=>codes=>visualize=>summary for one chapter, update to scatter and cluster information, add English explanation

--- research_notebook/research_kit.py
+++ research_notebook/research_kit.py
@@ -5,6 +5,8 @@ sys.path.append("../tradingbot")
 from datetime import datetime, timedelta
 from pprint import pprint
 
+from sklearn import linear_model
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
