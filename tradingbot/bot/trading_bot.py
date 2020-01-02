4d8e7bd5a3df64caab02d3fe5b441c1afea1e570
tori-takashi <tori.toutoshi@gmail.com>
2020-01-02 20:58:43 +0800

    remove bot identity

--- tradingbot/bot/trading_bot.py
+++ tradingbot/bot/trading_bot.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:32170b46f41f10b051e5471073ab4ad6a26cd2da8dbda6e4d89f17713cecbb23
-size 40406
+oid sha256:62004719cb5f6d410ade96a674ab52a0629d28c240ed1f1602ed834787df5261
+size 40269
--- tradingbot/run_bottom_trend_follow_on_real.py
+++ tradingbot/run_bottom_trend_follow_on_real.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ccd34da9dbd6f95328058f5cb017ad946e613a9666e557d32a0d7e722d644c3
-size 1172
+oid sha256:4f3f85ca1412c427377ec9ddae6bb4bb7b9db013e3bef0f4f45e2684543ccbcc
+size 1081
