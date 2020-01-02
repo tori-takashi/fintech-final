f0fba363aaa2276bb1018827bff6c36892c82616
tori-takashi <tori.toutoshi@gmail.com>
2020-01-02 19:48:44 +0800

    add order template, change to record bot using indicators, add influxdb columns to record transaction log

--- tradingbot/bot/trading_bot.py
+++ tradingbot/bot/trading_bot.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50a8b8adb92614874a1a926c275b96f9a14296f34c1e09517d8eef9709e98204
-size 34486
+oid sha256:87e48848c19aa5b5e94019feb5327e36cbb34eb16b402acb063cd8213bb8b799
+size 40258
