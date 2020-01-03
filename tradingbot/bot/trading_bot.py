a442f1dc67cc9a049a1bd935e78d543c761607e3
tori-takashi <tori.toutoshi@gmail.com>
2020-01-03 23:14:54 +0800

    add influxdb log functions, ver1.0.0 works on real environment

--- tradingbot/bot/trading_bot.py
+++ tradingbot/bot/trading_bot.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35edcdf893d642bf1d624ffcb8fe08978e8331fded5d529aecbd14035a5c16f0
-size 53748
+oid sha256:7a2f6c980f601a10531d6c0addc9123f8e5ff2e9850c35e017c3586ab44a9a7a
+size 53959
