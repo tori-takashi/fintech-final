c0e281ebe8ee26066db83a3e910afa40e4d1da33
tori-takashi <tori.toutoshi@gmail.com>
2020-01-03 18:42:32 +0800

    add line notification

--- tradingbot/bot/trading_bot.py
+++ tradingbot/bot/trading_bot.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6c628b026b99d1a550ccf19fb19a51caeb64668470f8b959838a56723ab9bed
-size 53504
+oid sha256:1bd33a537976810dc7cdea1a52275a82df90133d6b58f051e538078fc07466c6
+size 54670
--- tradingbot/client/db_client.py
+++ tradingbot/client/db_client.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b500bb4d76ea2154376a930850a8fa78b4e84dbc83086b6bcb28b0c68c4a92df
-size 5036
+oid sha256:f939f1a831a79d54e7788f12cd2478a7cf34fe50c086b83046f103db790ebeec
+size 5075
