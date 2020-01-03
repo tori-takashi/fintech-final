82ce9253123ed95c7cf46dcf2fb9de4a47437a2f
tori-takashi <tori.toutoshi@gmail.com>
2020-01-03 20:29:16 +0800

    fix missing order

--- tradingbot/client/db_client.py
+++ tradingbot/client/db_client.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ab17e2ce7e4ccec0b4a01366cc54c69e9ae749b7600168577fe44cbce249a40
-size 5610
+oid sha256:a7be0ceb575579a69b6416fb50023091296cc6a0159fb39e210a9dc1daa726f6
+size 5627
