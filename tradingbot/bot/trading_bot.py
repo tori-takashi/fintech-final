26e436a7f25f8a14467bddd151e1c1e8ac81a3d8
tori-takashi <tori.toutoshi@gmail.com>
2020-01-03 20:20:39 +0800

    take log

--- tradingbot/bot/trading_bot.py
+++ tradingbot/bot/trading_bot.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cae8f6ec713ca10346fb9048d94f514f6a730a44b70f2e04173fc1e81f0f8983
-size 53481
+oid sha256:35edcdf893d642bf1d624ffcb8fe08978e8331fded5d529aecbd14035a5c16f0
+size 53748
