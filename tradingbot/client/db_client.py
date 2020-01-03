537a8e613b9b8123fef615009002afdd0dc2bd8d
tori-takashi <tori.toutoshi@gmail.com>
2020-01-03 19:36:36 +0800

    add raw connector on influxdb

--- tradingbot/client/db_client.py
+++ tradingbot/client/db_client.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f939f1a831a79d54e7788f12cd2478a7cf34fe50c086b83046f103db790ebeec
-size 5075
+oid sha256:1ab17e2ce7e4ccec0b4a01366cc54c69e9ae749b7600168577fe44cbce249a40
+size 5610
