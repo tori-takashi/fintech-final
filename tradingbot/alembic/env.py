8fb2a2a50f0f30d53e395bc731e4cff275ba111f
tori-takashi <tori.toutoshi@gmail.com>
2020-01-06 01:46:34 +0800

    alembic

---
+++ tradingbot/alembic/env.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9e5e285ecafb2ee0d8e232264ad3eaae85dd457026f2c2cc38d73a069b76aa35
+size 2076
---
+++ tradingbot/alembic/versions/32ba1f11caba_base_revision.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:25606119301459f38b9f6af35dc6d3e5029593dbdccf27a3b7b8a5caa58a9648
+size 3605
