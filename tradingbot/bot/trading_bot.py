28a7047710acd91beb36a2605e532dcc216570a8
tori-takashi <tori.toutoshi@gmail.com>
2020-01-05 18:23:18 +0800

    integrate to backtest functions, combine signal judge

--- tradingbot/bot/trading_bot.py
+++ tradingbot/bot/trading_bot.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9cc5a40bf867ac71aabb47d85652706b4d1eef7b6526d2ab99eb0d89d6f9b239
-size 52186
+oid sha256:36fdb65927efaf35f53f1ed9aa0d1f4e01a205cdf33fd63e489bbe708ec2cacf
+size 48488
--- tradingbot/lib/dataset.py
+++ tradingbot/lib/dataset.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e493ffbac1f4a3625c44adc19dd51db8dbc69853820e60269883fc293c8dfb1c
-size 10760
+oid sha256:854267f39e5b97f76ca99a1f3b9b9e7e9aa717b5bc161680de0b438ab0f092aa
+size 11004
