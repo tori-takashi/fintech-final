a0ae1a4770f0c777fbbbe08782c88426243ff770
tori-takashi <tori.toutoshi@gmail.com>
2020-01-06 16:52:54 +0800

    update rk

--- research_notebook/research_kit_template.py
+++ research_notebook/research_kit_template.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3482c8c9bdf04ca7f2d642785558dd538ccaed71c8d0a33d18a113229eb73435
+oid sha256:df920cff6d48cd207657f0eadc10add2734d9316155d864711f93bcda3117743
 size 1269
