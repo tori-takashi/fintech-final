7720665d6c7f32eda03de4440c287c48300f501d
tori-takashi <tori.toutoshi@gmail.com>
2020-01-09 02:46:08 +0800

    fix missing

--- tradingbot/machine_learning/random_forest_prediction.py
+++ tradingbot/machine_learning/random_forest_prediction.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76e0afeada230c8570e6132fb1b160b93de634b5211f6a57e3d9ce5e47ee15ed
-size 2412
+oid sha256:dd7336a1deb4b058bda2e02adacc8acd87d0fde626a3d567a57e23e1516fb789
+size 2417
