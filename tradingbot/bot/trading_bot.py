81e1bb0441cfa4f50d21330d236bef6796b5a048
tori-takashi <tori.toutoshi@gmail.com>
2020-01-08 20:42:14 +0800

    remove orderposition due to splitting, fix maker and taker order

--- tradingbot/bot/trading_bot.py
+++ tradingbot/bot/trading_bot.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8d8324531b7c92d33f108938fc06b3d1454897b498a75f14b309ab891b66fd2
-size 52423
+oid sha256:5928be7eccb5cbd2b4d943cdecd59b1412bc1fa2164397f1d0c3b33a38c460a2
+size 42355
