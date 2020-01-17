b64731f91f9dbb42ab355c50cc4353e6d08aa3bb
tori-takashi <tori.toutoshi@gmail.com>
2020-01-17 14:43:40 +0900

    refactoring update function

--- tradingbot/lib/dataset.py
+++ tradingbot/lib/dataset.py
@@ -51,65 +51,27 @@ class Dataset:
         return pd.concat(ohlcv_df_list)
 
     def update_ohlcv(self, data_provider_name, start_time=None, asset_name=None, with_ta=False):
-        # if ohlcv table is existing, start time will be ignored
-        if self.db_client.is_mysql():
-            if self.db_client.is_table_exist(self.original_ohlcv_1min_table):
-                latest_row = self.db_client.get_last_row(
-                    self.original_ohlcv_1min_table)
-                if latest_row is True and latest_row.empty is False:
-                    start_time = self.calc_fetch_start_time(latest_row)
-            else:
-                self.build_ohlcv_1min_table()
+        while True:
+            download_start = datetime.now()
 
-        if self.db_client.is_influxdb():
-            # {FIXME} Hard coding
-            if self.db_client.is_table_exist("OHLCV_data"):
-                latest_row = self.db_client.get_last_row_with_tags(
-                    "OHLCV_data", {"exchange_name": "bitmex", "asset_name": "BTC/USD"})
-                start_time = self.calc_fetch_start_time(latest_row)
+            latest_row = self.get_latest_row()
+            start_time = self.calc_fetch_start_time(
+                latest_row) if latest_row else start_time
 
-        if data_provider_name == "bitmex":
-            ohlcv_df = self.download_ohlcv_data_from_bitmex(
-                "1m", start_time, end_time=datetime.now())
+            if data_provider_name == "bitmex":
+                ohlcv_df = self.download_ohlcv_data_from_bitmex(
+                    "1m", start_time, end_time=datetime.now())
 
-        if with_ta and ohlcv_df.empty is not True:
-            if self.db_client.is_influxdb() and self.db_client.is_table_exist("OHLCV_data"):
-                padding_df = self.get_ohlcv(timeframe=1, start_time=datetime.now() - timedelta(minutes=200),
-                                            end_time=datetime.now(), round=False)
+            if with_ta and ohlcv_df.empty is not True:
+                ohlcv_df = self.attach_technical_indicators_for_update_data(
+                    ohlcv_df, start_time)
 
-                ohlcv_df["timestamp"] = ohlcv_df["timestamp"].dt.tz_localize(
-                    timezone.utc)
-                ohlcv_df.set_index('timestamp', inplace=True)
+            self.append_update_data(ohlcv_df)
 
-                concatnated_df = pd.concat(
-                    [padding_df, ohlcv_df], axis=0, sort=False)
-                concatnated_df.index = concatnated_df.index.map(np.datetime64)
+            if datetime.now() - download_start < timedelta(seconds=29):
+                break
 
-                ohlcv_df = self.add_technical_statistics_to_ohlcv_df(
-                    concatnated_df)
-
-            elif self.db_client.is_mysql() and self.db_client.is_table_exist(self.original_ohlcv_1min_table):
-                padding_df = self.get_ohlcv(timeframe=1, start_time=start_time - timedelta(minutes=60),
-                                            end_time=start_time, round=False)
-
-                if padding_df is not None and not padding_df.empty:
-                    ohlcv_df.set_index('timestamp', inplace=True)
-
-                    concatnated_df = pd.concat(
-                        [padding_df, ohlcv_df], axis=0, sort=False)
-
-                    ohlcv_df = self.add_technical_statistics_to_ohlcv_df(
-                        concatnated_df)
-
-                ohlcv_df = self.add_technical_statistics_to_ohlcv_df(
-                    ohlcv_df)
-                ohlcv_df.set_index("timestamp", inplace=True)
-                print(ohlcv_df)
-                ohlcv_df = ohlcv_df[start_time:]
-            else:  # ?
-                ohlcv_df = self.add_technical_statistics_to_ohlcv_df(
-                    ohlcv_df)
-
+    def append_update_data(self, ohlcv_df):
         if self.db_client.is_mysql():
             self.db_client.append_to_table(
                 self.original_ohlcv_1min_table, ohlcv_df)
@@ -121,6 +83,56 @@ class Dataset:
             self.db_client.append_to_table(
                 "OHLCV_data", ohlcv_df
             )
+
+    def attach_technical_indicators_for_update_data(self, ohlcv_df, start_time):
+        padding_df = self.get_ohlcv(timeframe=1, start_time=datetime.now() - timedelta(minutes=200),
+                                    end_time=datetime.now(), round=False)
+
+        if self.db_client.is_influxdb() and self.db_client.is_table_exist("OHLCV_data"):
+            ohlcv_df["timestamp"] = ohlcv_df["timestamp"].dt.tz_localize(
+                timezone.utc)
+            ohlcv_df.set_index('timestamp', inplace=True)
+
+            concatnated_df = pd.concat(
+                [padding_df, ohlcv_df], axis=0, sort=False)
+            concatnated_df.index = concatnated_df.index.map(
+                np.datetime64)
+
+            return self.add_technical_statistics_to_ohlcv_df(
+                concatnated_df)
+
+        elif self.db_client.is_mysql() and self.db_client.is_table_exist(self.original_ohlcv_1min_table):
+            if padding_df is not None and not padding_df.empty:
+                ohlcv_df.set_index('timestamp', inplace=True)
+
+                concatnated_df = pd.concat(
+                    [padding_df, ohlcv_df], axis=0, sort=False)
+
+                ohlcv_df = self.add_technical_statistics_to_ohlcv_df(
+                    concatnated_df)
+
+            ohlcv_df = self.add_technical_statistics_to_ohlcv_df(
+                ohlcv_df)
+            ohlcv_df.set_index("timestamp", inplace=True)
+            print(ohlcv_df)
+            return ohlcv_df[start_time:]
+
+    def get_latest_row(self):
+        if self.db_client.is_mysql():
+            if self.db_client.is_table_exist(self.original_ohlcv_1min_table):
+                latest_row = self.db_client.get_last_row(
+                    self.original_ohlcv_1min_table)
+                if latest_row is True and latest_row.empty is False:
+                    return latest_row
+            else:
+                self.build_ohlcv_1min_table()
+
+        elif self.db_client.is_influxdb():
+            # {FIXME} Hard coding
+            if self.db_client.is_table_exist("OHLCV_data"):
+                latest_row = self.db_client.get_last_row_with_tags(
+                    "OHLCV_data", {"exchange_name": "bitmex", "asset_name": "BTC/USD"})
+                return latest_row
 
     def add_technical_statistics_to_ohlcv_df(self, df):
         ta_ad = TechnicalAnalysisAD(df)
