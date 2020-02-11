e4a9317249fb07369811add17cc5df3c9915d2d7
tori-takashi <tori.toutoshi@gmail.com>
2020-02-11 17:17:07 +0900

    template

---
+++ tradingbot/bot/turtles_breakout.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import pathlib
+from datetime import datetime, timedelta
+from sqlalchemy import Column, Integer
+
+from bot.trading_bot import TradingBot
+
+
+class TurtlesBreakout():
+    def __init__(self):
+        pass
